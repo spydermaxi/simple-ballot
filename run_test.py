147da1ce6a5ac35f311686605c8f310409fee134
Adrian Loo <adrian.loo1981@gmail.com>
2022-04-30 07:57:41 +0800

    developed class. to be continued

--- run_test.py
+++ run_test.py
@@ -1,33 +1,3 @@
-# from simple_ballot import ballot
+from simple_ballot import simple_ballot as sb
 
-# INPUT_FILE = r".\test\applicants.csv"
-
-# output = ballot.run_draw(input_path=INPUT_FILE, total_resource=30, ignore_duplicates=False)
-
-import getopt
-import sys
-import io
-
-try:
-    opts, args = getopt.getopt(
-        sys.argv[1:],
-        "h1o:s:F:A:f:",
-        ["help", "header", "output", "sep=", "float=", "align=", "format="],
-    )
-except getopt.GetoptError as e:
-    print(e)
-    print(usage)
-    sys.exit(2)
-
-files = [sys.stdin] if not args else args
-
-
-input(getopt.getopt(
-    sys.argv[1:],
-    "h1o:s:F:A:f:",
-    ["help", "header", "output", "sep=", "float=", "align=", "format="],
-))
-for opt, value in opts:
-    print(opt)
-input(args)
-input(files)
+sb.System(r".\test\applicants.csv").draw(resource=20)
